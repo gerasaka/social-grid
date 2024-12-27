c4f7f7f9c1cb7e52c950619c3561ce74f472b760
gerry.julio <gerry.julio@doku.com>
2024-12-27 15:46:07 +0700

    refactor: initialize api call in the app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+import { forkJoin } from 'rxjs';
+import { ListLoadingComponent } from './components/list-loading.component';
+import { ApiService } from './core/services/api/api.service';
+import { StorageService } from './core/services/storage/storage.service';
+import { NavbarComponent } from './layouts/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, ListLoadingComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'social-grid';
+export class AppComponent implements OnInit {
+  private apiService = inject(ApiService);
+  private storageService = inject(StorageService);
+  private router = inject(Router);
+
+  pageState: 'LOADING' | 'COMPLETE' = 'LOADING';
+
+  ngOnInit(): void {
+    forkJoin([
+      this.apiService.getAllPosts$(),
+      this.apiService.getAllAlbums$(),
+      this.apiService.getAllPhotos$(),
+    ]).subscribe({
+      next: ([posts, albums, photos]) => {
+        this.storageService.posts = posts;
+        this.storageService.albums = albums;
+        this.storageService.photos = photos;
+        this.pageState = 'COMPLETE';
+      },
+      error: () => this.router.navigate(['error']),
+    });
+
+    this.apiService.loadUsers();
+  }
 }
