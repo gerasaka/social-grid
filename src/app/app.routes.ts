771cbc04203eb20f2c3d3f0d1d6d3953cf887b23
gerry.julio <gerry.julio@doku.com>
2024-12-27 15:03:07 +0700

    feat: add app route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,47 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'dashboard',
+  },
+  {
+    path: 'posts',
+    loadComponent: () => import('./pages/posts/posts.component').then((c) => c.PostsPages),
+  },
+  {
+    path: 'posts/:id',
+    loadComponent: () =>
+      import('./pages/post-details/post-details.component').then((c) => c.PostDetailsPage),
+  },
+  {
+    path: 'users/:id',
+    loadComponent: () =>
+      import('./pages/user-profile/user-profile.component').then((c) => c.UserProfilePage),
+  },
+  {
+    path: 'albums',
+    loadComponent: () => import('./pages/albums/albums.component').then((c) => c.AlbumsPage),
+  },
+  {
+    path: 'albums/:id',
+    loadComponent: () =>
+      import('./pages/album-details/album-details.component').then((c) => c.AlbumDetailsPage),
+  },
+  {
+    path: 'photos',
+    loadComponent: () => import('./pages/photos/photos.component').then((c) => c.PhotosPage),
+  },
+  {
+    path: 'photos/:id',
+    loadComponent: () =>
+      import('./pages/photo-details/photo-details.component').then((c) => c.PhotoDetailsPage),
+  },
+  {
+    path: 'error',
+    loadComponent: () => import('./pages/error/error.component').then((c) => c.ErrorPage),
+  },
+  {
+    path: '',
+    redirectTo: 'dashboard',
+  },
+];
