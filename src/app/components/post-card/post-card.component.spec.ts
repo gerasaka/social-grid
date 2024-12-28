cdca23418090005915dbf69d006905f9432e0203
gerry.julio <gerry.julio@doku.com>
2024-12-28 21:21:14 +0700

    test: post card component

--- src/app/components/post-card/post-card.component.spec.ts
+++ src/app/components/post-card/post-card.component.spec.ts
@@ -1,23 +1,57 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { ComponentRef } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { MOCK_BOOKMARKED } from '../../core/sample-response/bookmark';
+import { MOCK_POST } from '../../core/sample-response/post';
+import { MOCK_USER_LIST } from '../../core/sample-response/user';
+import { StorageService } from '../../core/services/storage/storage.service';
 import { PostCardComponent } from './post-card.component';
 
 describe('PostCardComponent', () => {
   let component: PostCardComponent;
+  let componentRef: ComponentRef<PostCardComponent>;
   let fixture: ComponentFixture<PostCardComponent>;
+  let storageService: jasmine.SpyObj<StorageService>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [PostCardComponent]
-    })
-    .compileComponents();
+  beforeEach(() => {
+    storageService = jasmine.createSpyObj('StorageService', [
+      'users',
+      'bookmarkedPosts',
+      'addBookmark',
+      'removeBookmark',
+    ]);
+
+    TestBed.configureTestingModule({
+      providers: [provideRouter([]), { provide: StorageService, useValue: storageService }],
+    });
 
     fixture = TestBed.createComponent(PostCardComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    componentRef = fixture.componentRef;
+    componentRef.setInput('post', MOCK_POST);
+
+    storageService.users.and.returnValue(MOCK_USER_LIST);
+    storageService.bookmarkedPosts.and.returnValue(MOCK_BOOKMARKED);
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should retrieve post information', () => {
+    const info = component.getPostInfo(MOCK_POST);
+
+    expect(info).toEqual({ author: MOCK_USER_LIST[MOCK_POST.userId - 1].name, bookmarked: true });
+    expect(storageService.users).toHaveBeenCalled();
+    expect(storageService.bookmarkedPosts).toHaveBeenCalled();
+  });
+
+  it('should bookmark a post', () => {
+    component.bookmarkPost(MOCK_POST);
+
+    expect(storageService.addBookmark).toHaveBeenCalledWith(MOCK_POST);
+  });
+
+  it('should remove a bookmarked post', () => {
+    component.removeBookmark(MOCK_POST.id);
+
+    expect(storageService.removeBookmark).toHaveBeenCalledWith(MOCK_POST.id);
   });
 });
