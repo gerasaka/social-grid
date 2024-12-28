e4d142fec3d4f0b121fef67bbc544984689c58df
gerry.julio <gerry.julio@doku.com>
2024-12-28 11:40:43 +0700

    chore: add mock sample response

---
+++ src/app/core/sample-response/album.ts
@@ -0,0 +1,28 @@
+export const MOCK_ALBUM_LIST = [
+  {
+    userId: 1,
+    id: 1,
+    title: 'quidem molestiae enim',
+  },
+  {
+    userId: 2,
+    id: 11,
+    title: 'quam nostrum impedit mollitia quod et dolor',
+  },
+  {
+    userId: 3,
+    id: 21,
+    title: 'repudiandae voluptatem optio est consequatur rem in temporibus et',
+  },
+  {
+    userId: 4,
+    id: 31,
+    title: 'adipisci laborum fuga laboriosam',
+  },
+];
+
+export const MOCK_ALBUM = {
+  userId: 1,
+  id: 1,
+  title: 'quidem molestiae enim',
+};
---
+++ src/app/core/sample-response/bookmark.ts
@@ -0,0 +1,26 @@
+export const MOCK_BOOKMARKED = {
+  '1': {
+    userId: 1,
+    id: 1,
+    title: 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
+    body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto',
+  },
+  '2': {
+    userId: 1,
+    id: 2,
+    title: 'qui est esse',
+    body: 'est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla',
+  },
+  '11': {
+    userId: 2,
+    id: 11,
+    title: 'et ea vero quia laudantium autem',
+    body: 'delectus reiciendis molestiae occaecati non minima eveniet qui voluptatibus\naccusamus in eum beatae sit\nvel qui neque voluptates ut commodi qui incidunt\nut animi commodi',
+  },
+  '12': {
+    userId: 2,
+    id: 12,
+    title: 'in quibusdam tempore odit est dolorem',
+    body: 'itaque id aut magnam\npraesentium quia et ea odit et ea voluptas et\nsapiente quia nihil amet occaecati quia id voluptatem\nincidunt ea est distinctio odio',
+  },
+};
