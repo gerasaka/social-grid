98ada98ccf7a67c874b01cb15bc5553d16c17820
gerry.julio <gerry.julio@doku.com>
2024-12-28 21:23:14 +0700

    test: photo card component

--- src/app/components/photo-card/photo-card.component.spec.ts
+++ src/app/components/photo-card/photo-card.component.spec.ts
@@ -1,19 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { ComponentRef } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { MOCK_PHOTO } from '../../core/sample-response/photos';
 import { PhotoCardComponent } from './photo-card.component';
 
 describe('PhotoCardComponent', () => {
   let component: PhotoCardComponent;
+  let componentRef: ComponentRef<PhotoCardComponent>;
   let fixture: ComponentFixture<PhotoCardComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PhotoCardComponent]
-    })
-    .compileComponents();
+      imports: [PhotoCardComponent],
+      providers: [provideRouter([])],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(PhotoCardComponent);
     component = fixture.componentInstance;
+    componentRef = fixture.componentRef;
+    componentRef.setInput('photo', MOCK_PHOTO);
+
     fixture.detectChanges();
   });
 
