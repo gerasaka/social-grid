19f7d5cef23940bf32e0b699ccc57fd73518b0a3
gerry.julio <gerry.julio@doku.com>
2024-12-30 10:21:33 +0700

    refactor: implement list container in photos page

--- src/app/pages/photos/photos.component.spec.ts
+++ src/app/pages/photos/photos.component.spec.ts
@@ -1,8 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ActivatedRoute, provideRouter, Router } from '@angular/router';
+import { ActivatedRoute, provideRouter } from '@angular/router';
 import { of } from 'rxjs';
 import { MOCK_PHOTO_LIST } from '../../core/sample-response/photos';
+import { IPhoto } from '../../core/services/api/response.dto';
 import { StorageService } from '../../core/services/storage/storage.service';
 import { PhotosPage } from './photos.component';
 
@@ -10,7 +11,6 @@ describe('PhotosPage', () => {
   let component: PhotosPage;
   let fixture: ComponentFixture<PhotosPage>;
   let storageService: jasmine.SpyObj<StorageService>;
-  let router: Router;
   let activeRoute: ActivatedRoute;
 
   beforeEach(() => {
@@ -23,21 +23,19 @@ describe('PhotosPage', () => {
 
     fixture = TestBed.createComponent(PhotosPage);
     component = fixture.componentInstance;
-    router = TestBed.inject(Router);
     activeRoute = TestBed.inject(ActivatedRoute);
   });
 
-  it('should subscribe to route queryParams and update filters', () => {
+  it('should set page filter and state from query params', () => {
     spyOn(component, 'loadPhotos');
+    activeRoute.queryParams = of({ search: 'test', sort: 'DESC', page: 2 });
 
-    activeRoute.queryParams = of({ search: 'Example', sort: 'DESC', page: 2 });
+    fixture.detectChanges();
 
-    component.ngOnInit();
-
-    expect(component.search).toBe('Example');
-    expect(component.sort).toBe('DESC');
-    expect(component.currPage).toBe(2);
-    expect(component.loadPhotos).toHaveBeenCalledWith('Example', 'DESC');
+    expect(component.pagefilter.search).toBe('test');
+    expect(component.pagefilter.sort).toBe('DESC');
+    expect(component.pageState.currPage).toBe(2);
+    expect(component.loadPhotos).toHaveBeenCalledWith('test', 'DESC');
   });
 
   it('should handle default values for missing queryParams', () => {
@@ -45,93 +43,51 @@ describe('PhotosPage', () => {
 
     component.ngOnInit();
 
-    expect(component.search).toBe('');
-    expect(component.sort).toBe('DEFAULT');
-    expect(component._currPage).toBe(1);
+    expect(component.pagefilter.search).toBe('');
+    expect(component.pagefilter.sort).toBe('DEFAULT');
+    expect(component.pageState.currPage).toBe(1);
   });
 
-  it('should search and sort albums with the given searchquery and sortquery ', () => {
-    spyOn(component, 'searchPhotos');
-    spyOn(component, 'sortPhotos');
+  it('should filter posts based on search query', () => {
+    component.loadPhotos('non', undefined);
 
-    component.loadPhotos('test', 'ASC');
-
-    expect(component.searchPhotos).toHaveBeenCalledWith('test');
-    expect(component.sortPhotos).toHaveBeenCalledWith('ASC');
-    expect(component.loading).toBeFalse();
+    expect(component.filteredPhotos.every((photo) => photo.title.includes('non'))).toBeTrue();
   });
 
-  it('should load photos and set filteredPhotos correctly', () => {
-    component.loadPhotos();
+  it('should sort posts in ascending order', () => {
+    const sortCallback = (a: IPhoto, b: IPhoto) => a.title[0].localeCompare(b.title[0]);
 
-    expect(component.filteredPhotos()).toEqual(MOCK_PHOTO_LIST);
-    expect(component.totalPhoto).toBe(MOCK_PHOTO_LIST.length);
-    expect(component.loading).toBeFalse();
+    component.loadPhotos(undefined, 'ASC');
+
+    expect(component.filteredPhotos).toEqual(component.filteredPhotos.sort(sortCallback));
   });
 
-  it('should apply filters and update queryParams', () => {
-    const applyFiltersSpy = spyOn(component, 'applyFilters').and.callThrough();
-    const routerSpy = spyOn(router, 'navigate');
+  it('should sort posts in descending order', () => {
+    const sortCallback = (a: IPhoto, b: IPhoto) => b.title[0].localeCompare(a.title[0]);
 
-    component.search = 'non';
-    component.sort = 'ASC';
-    component.currPage = 2;
+    component.loadPhotos(undefined, 'DESC');
 
-    component.applyFilters({ search: 'non', sort: 'ASC' });
-
-    expect(applyFiltersSpy).toHaveBeenCalledWith({ search: 'non', sort: 'ASC' });
-    expect(routerSpy).toHaveBeenCalledWith(['photos'], {
-      queryParams: { search: 'non', sort: 'ASC', page: 2 },
-    });
-  });
-
-  it('should reset filters if searchQuery and sortQuery is not given', () => {
-    const routerSpy = spyOn(router, 'navigate');
-
-    component.applyFilters({ search: null, sort: 'DEFAULT' });
-
-    expect(routerSpy).toHaveBeenCalledWith(['photos'], {
-      queryParams: { search: undefined, sort: undefined, page: 1 },
-    });
-  });
-
-  it('should update photos signal based on filteredPhotos', () => {
-    component.filteredPhotos.set(MOCK_PHOTO_LIST);
-
-    expect(component.photos()).toEqual(MOCK_PHOTO_LIST.slice(0, component.pageSize));
-  });
-
-  it('should filter photos by search term', () => {
-    component.searchPhotos('non');
-
-    const filteredPhotos = component.filteredPhotos();
-
-    expect(filteredPhotos.length).toBeLessThan(MOCK_PHOTO_LIST.length);
-    expect(filteredPhotos[0].title).toContain('non');
-  });
-
-  it('should sort photos in ascending order', () => {
-    component.loadPhotos();
-    component.sortPhotos('ASC');
-
-    const sortedPhotos = component.filteredPhotos();
-
-    expect(sortedPhotos[0].title.localeCompare(sortedPhotos[1].title)).toBeLessThanOrEqual(0);
-  });
-
-  it('should sort albums in descending order', () => {
-    component.loadPhotos();
-    component.sortPhotos('DESC');
-
-    const sortedPhotos = component.filteredPhotos();
-
-    expect(sortedPhotos[0].title.localeCompare(sortedPhotos[1].title)).toBeGreaterThanOrEqual(0);
+    expect(component.filteredPhotos).toEqual(component.filteredPhotos.sort(sortCallback));
   });
 
   it('should sort albums in default order', () => {
     component.loadPhotos();
     component.sortPhotos('DEFAULT');
 
-    expect(component.filteredPhotos()).toEqual(MOCK_PHOTO_LIST);
+    expect(component.filteredPhotos).toEqual(MOCK_PHOTO_LIST);
+  });
+
+  it('should paginate posts correctly', () => {
+    const page = 2;
+    component.pageState.currPage = page;
+    component.pageState.pageSize = 3;
+
+    component.loadPhotos();
+
+    const start = (page - 1) * component.pageState.pageSize;
+    const end = start + component.pageState.pageSize;
+
+    expect(component.photos).toEqual(MOCK_PHOTO_LIST.slice(start, end));
+    expect(component.pageState.totalContent).toBe(MOCK_PHOTO_LIST.length);
   });
 });
--- src/app/pages/photos/photos.component.ts
+++ src/app/pages/photos/photos.component.ts
@@ -1,112 +1,86 @@
 import { CommonModule } from '@angular/common';
-import { Component, computed, DestroyRef, inject, OnInit, signal } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { ReactiveFormsModule } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
-import { NgbPagination } from '@ng-bootstrap/ng-bootstrap';
-import { FilterComponent } from '../../components/filter/filter.component';
-import { TFilter, TSortQuery } from '../../components/filter/filter.types';
-import { ListLoadingComponent } from '../../components/list-loading.component';
+import { ActivatedRoute } from '@angular/router';
+import { TSortQuery } from '../../components/filter/filter.types';
 import { PhotoCardComponent } from '../../components/photo-card/photo-card.component';
 import { IPhoto } from '../../core/services/api/response.dto';
 import { StorageService } from '../../core/services/storage/storage.service';
+import { ListContainerComponent } from '../../layouts/list-container/list-container.component';
+import { TPageFilter, TPageState } from '../../layouts/list-container/list-container.type';
 
 @Component({
   selector: 'photos',
-  imports: [
-    CommonModule,
-    ReactiveFormsModule,
-    ListLoadingComponent,
-    PhotoCardComponent,
-    FilterComponent,
-    NgbPagination,
-  ],
+  imports: [CommonModule, ReactiveFormsModule, ListContainerComponent, PhotoCardComponent],
   templateUrl: './photos.component.html',
   styleUrl: './photos.component.scss',
 })
 export class PhotosPage implements OnInit {
   private storageService = inject(StorageService);
   private activeRoute = inject(ActivatedRoute);
-  private router = inject(Router);
 
   readonly #destroyRef = inject(DestroyRef);
 
-  loading = true;
-  _currPage = 1;
-  pageSize = 50;
-  totalPhoto = 0;
+  pageState: TPageState = {
+    loading: true,
+    currPage: 1,
+    pageSize: 54,
+    totalContent: 0,
+  };
 
-  search = '';
-  sort: TSortQuery = 'DEFAULT';
+  pagefilter: TPageFilter = {
+    search: '',
+    sort: 'DEFAULT',
+  };
 
-  filteredPhotos = signal<IPhoto[]>([]);
-  photos = computed(() => {
-    const start = (this._currPage - 1) * this.pageSize;
-    const end = start + this.pageSize;
-    return this.filteredPhotos().slice(start, end);
-  });
+  filteredPhotos: IPhoto[] = [];
+  photos: IPhoto[] = [];
 
   ngOnInit(): void {
     this.activeRoute.queryParams.pipe(takeUntilDestroyed(this.#destroyRef)).subscribe({
       next: ({ search, sort, page }) => {
-        this.search = search || '';
-        this.sort = sort || 'DEFAULT';
-        this._currPage = page || 1;
+        this.pagefilter.search = search || '';
+        this.pagefilter.sort = sort || 'DEFAULT';
+        this.pageState.currPage = page || 1;
 
         this.loadPhotos(search, sort);
       },
     });
   }
 
-  get currPage() {
-    return this._currPage;
-  }
-
-  set currPage(page: number) {
-    this._currPage = page;
-    this.applyFilters({ search: this.search, sort: this.sort });
-  }
-
   loadPhotos(query?: string, sort?: string) {
-    this.filteredPhotos.set([...this.storageService.photos]);
+    this.filteredPhotos = [...this.storageService.photos];
 
     if (query) this.searchPhotos(query);
     if (sort) this.sortPhotos(sort as TSortQuery);
 
-    this.totalPhoto = this.filteredPhotos().length;
-    this.loading = false;
-  }
+    const start = (this.pageState.currPage - 1) * this.pageState.pageSize;
+    const end = start + this.pageState.pageSize;
 
-  applyFilters({ search, sort }: TFilter) {
-    this.router.navigate(['photos'], {
-      queryParams: {
-        search: search || undefined,
-        sort: sort === 'DEFAULT' ? undefined : sort,
-        page: this.currPage,
-      },
-    });
+    this.photos = this.filteredPhotos.slice(start, end);
+    this.pageState.totalContent = this.filteredPhotos.length;
+    this.pageState.loading = false;
   }
 
   searchPhotos(query: string) {
-    this.filteredPhotos.set(
-      this.storageService.photos.filter((photo) => photo.title.includes(query)),
-    );
+    this.filteredPhotos = this.storageService.photos.filter((photo) => photo.title.includes(query));
   }
 
   sortPhotos(sort: TSortQuery) {
     switch (sort) {
       case 'ASC':
-        this.filteredPhotos.update((photo) =>
-          photo.sort((a, b) => a.title[0].localeCompare(b.title[0])),
+        this.filteredPhotos = this.filteredPhotos.sort((a, b) =>
+          a.title[0].localeCompare(b.title[0]),
         );
         break;
       case 'DESC':
-        this.filteredPhotos.update((photo) =>
-          photo.sort((a, b) => b.title[0].localeCompare(a.title[0])),
+        this.filteredPhotos = this.filteredPhotos.sort((a, b) =>
+          b.title[0].localeCompare(a.title[0]),
         );
         break;
       default:
-        this.filteredPhotos.update((photo) => photo.sort((a, b) => a.id - b.id));
+        this.filteredPhotos = this.filteredPhotos.sort((a, b) => a.id - b.id);
         break;
     }
   }
