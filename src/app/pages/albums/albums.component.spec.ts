a0b43861d530ce106663b5818a1cbc934c92fd45
gerry.julio <gerry.julio@doku.com>
2024-12-30 10:07:21 +0700

    refactor: implement list container in albums page

--- src/app/pages/albums/albums.component.spec.ts
+++ src/app/pages/albums/albums.component.spec.ts
@@ -1,9 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ActivatedRoute, provideRouter, Router } from '@angular/router';
+import { ActivatedRoute, provideRouter } from '@angular/router';
 import { of } from 'rxjs';
 import { MOCK_ALBUM_LIST } from '../../core/sample-response/album';
 import { MOCK_USER_LIST } from '../../core/sample-response/user';
+import { IAlbum } from '../../core/services/api/response.dto';
 import { StorageService } from '../../core/services/storage/storage.service';
 import { AlbumsPage } from './albums.component';
 
@@ -11,7 +12,6 @@ describe('AlbumsPage', () => {
   let component: AlbumsPage;
   let fixture: ComponentFixture<AlbumsPage>;
   let storageService: jasmine.SpyObj<StorageService>;
-  let router: Router;
   let activeRoute: ActivatedRoute;
 
   beforeEach(() => {
@@ -24,21 +24,19 @@ describe('AlbumsPage', () => {
 
     fixture = TestBed.createComponent(AlbumsPage);
     component = fixture.componentInstance;
-    router = TestBed.inject(Router);
     activeRoute = TestBed.inject(ActivatedRoute);
   });
 
-  it('should subscribe to route queryParams and update filters', () => {
+  it('should set page filter and state from query params', () => {
     spyOn(component, 'loadAlbums');
+    activeRoute.queryParams = of({ search: 'test', sort: 'DESC', page: 2 });
 
-    activeRoute.queryParams = of({ search: 'Example', sort: 'DESC', page: 2 });
+    fixture.detectChanges();
 
-    component.ngOnInit();
-
-    expect(component.search).toBe('Example');
-    expect(component.sort).toBe('DESC');
-    expect(component.currPage).toBe(2);
-    expect(component.loadAlbums).toHaveBeenCalledWith('Example', 'DESC');
+    expect(component.pagefilter.search).toBe('test');
+    expect(component.pagefilter.sort).toBe('DESC');
+    expect(component.pageState.currPage).toBe(2);
+    expect(component.loadAlbums).toHaveBeenCalledWith('test', 'DESC');
   });
 
   it('should handle default values for missing queryParams', () => {
@@ -46,28 +44,38 @@ describe('AlbumsPage', () => {
 
     component.ngOnInit();
 
-    expect(component.search).toBe('');
-    expect(component.sort).toBe('DEFAULT');
-    expect(component._currPage).toBe(1);
+    expect(component.pagefilter.search).toBe('');
+    expect(component.pagefilter.sort).toBe('DEFAULT');
+    expect(component.pageState.currPage).toBe(1);
   });
 
-  it('should load albums and set filteredAlbums correctly', () => {
+  it('should filter posts based on search query', () => {
+    component.loadAlbums('quam', undefined);
+
+    expect(component.filteredAlbums.every((album) => album.title.includes('quam'))).toBeTrue();
+  });
+
+  it('should sort posts in ascending order', () => {
+    const sortCallback = (a: IAlbum, b: IAlbum) => a.title[0].localeCompare(b.title[0]);
+
+    component.loadAlbums(undefined, 'ASC');
+
+    expect(component.filteredAlbums).toEqual(component.filteredAlbums.sort(sortCallback));
+  });
+
+  it('should sort posts in descending order', () => {
+    const sortCallback = (a: IAlbum, b: IAlbum) => b.title[0].localeCompare(a.title[0]);
+
+    component.loadAlbums(undefined, 'DESC');
+
+    expect(component.filteredAlbums).toEqual(component.filteredAlbums.sort(sortCallback));
+  });
+
+  it('should sort albums in default order', () => {
     component.loadAlbums();
+    component.sortAlbums('DEFAULT');
 
-    expect(component.filteredAlbums()).toEqual(MOCK_ALBUM_LIST);
-    expect(component.totalAlbum).toBe(MOCK_ALBUM_LIST.length);
-    expect(component.loading).toBeFalse();
-  });
-
-  it('should search and sort albums with the given searchquery and sortquery ', () => {
-    spyOn(component, 'searchAlbums');
-    spyOn(component, 'sortAlbums');
-
-    component.loadAlbums('test', 'ASC');
-
-    expect(component.searchAlbums).toHaveBeenCalledWith('test');
-    expect(component.sortAlbums).toHaveBeenCalledWith('ASC');
-    expect(component.loading).toBeFalse();
+    expect(component.filteredAlbums).toEqual(MOCK_ALBUM_LIST);
   });
 
   it('should return the name of the user based on ID', () => {
@@ -79,68 +87,17 @@ describe('AlbumsPage', () => {
     expect(authorName).toBe('Ervin Howell');
   });
 
-  it('should apply filters and update queryParams', () => {
-    const applyFiltersSpy = spyOn(component, 'applyFilters').and.callThrough();
-    const routerSpy = spyOn(router, 'navigate');
+  it('should paginate posts correctly', () => {
+    const page = 2;
+    component.pageState.currPage = page;
+    component.pageState.pageSize = 3;
 
-    component.search = 'quam';
-    component.sort = 'ASC';
-    component.currPage = 2;
-    component.applyFilters({ search: 'quam', sort: 'ASC' });
-
-    expect(applyFiltersSpy).toHaveBeenCalledWith({ search: 'quam', sort: 'ASC' });
-    expect(routerSpy).toHaveBeenCalledWith(['albums'], {
-      queryParams: { search: 'quam', sort: 'ASC', page: 2 },
-    });
-  });
-
-  it('should reset filters if searchQuery and sortQuery is not given', () => {
-    const routerSpy = spyOn(router, 'navigate');
-
-    component.applyFilters({ search: null, sort: 'DEFAULT' });
-
-    expect(routerSpy).toHaveBeenCalledWith(['albums'], {
-      queryParams: { search: undefined, sort: undefined, page: 1 },
-    });
-  });
-
-  it('should update albums signal based on filteredAlbums', () => {
-    component.filteredAlbums.set(MOCK_ALBUM_LIST);
-
-    expect(component.albums()).toEqual(MOCK_ALBUM_LIST.slice(0, component.pageSize));
-  });
-
-  it('should filter albums by search term', () => {
-    component.searchAlbums('quam');
-
-    const filteredAlbums = component.filteredAlbums();
-
-    expect(filteredAlbums.length).toBeLessThan(MOCK_ALBUM_LIST.length);
-    expect(filteredAlbums[0].title).toContain('quam');
-  });
-
-  it('should sort albums in ascending order', () => {
     component.loadAlbums();
-    component.sortAlbums('ASC');
 
-    const sortedAlbums = component.filteredAlbums();
+    const start = (page - 1) * component.pageState.pageSize;
+    const end = start + component.pageState.pageSize;
 
-    expect(sortedAlbums[0].title.localeCompare(sortedAlbums[1].title)).toBeLessThanOrEqual(0);
-  });
-
-  it('should sort albums in descending order', () => {
-    component.loadAlbums();
-    component.sortAlbums('DESC');
-
-    const sortedAlbums = component.filteredAlbums();
-
-    expect(sortedAlbums[0].title.localeCompare(sortedAlbums[1].title)).toBeGreaterThanOrEqual(0);
-  });
-
-  it('should sort albums in default order', () => {
-    component.loadAlbums();
-    component.sortAlbums('DEFAULT');
-
-    expect(component.filteredAlbums()).toEqual(MOCK_ALBUM_LIST);
+    expect(component.albums).toEqual(MOCK_ALBUM_LIST.slice(start, end));
+    expect(component.pageState.totalContent).toBe(MOCK_ALBUM_LIST.length);
   });
 });
--- src/app/pages/albums/albums.component.ts
+++ src/app/pages/albums/albums.component.ts
@@ -1,81 +1,66 @@
 import { CommonModule } from '@angular/common';
-import { Component, computed, DestroyRef, inject, OnInit, signal } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { ReactiveFormsModule } from '@angular/forms';
-import { ActivatedRoute, Router, RouterLink } from '@angular/router';
-import { NgbPagination } from '@ng-bootstrap/ng-bootstrap';
-import { FilterComponent } from '../../components/filter/filter.component';
-import { TFilter, TSortQuery } from '../../components/filter/filter.types';
-import { ListLoadingComponent } from '../../components/list-loading.component';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { TSortQuery } from '../../components/filter/filter.types';
 import { IAlbum } from '../../core/services/api/response.dto';
 import { StorageService } from '../../core/services/storage/storage.service';
+import { ListContainerComponent } from '../../layouts/list-container/list-container.component';
+import { TPageFilter, TPageState } from '../../layouts/list-container/list-container.type';
 import { CircleUserIcon } from '../../shared/icons/circle-user.component';
 
 @Component({
   selector: 'albums',
-  imports: [
-    CommonModule,
-    ReactiveFormsModule,
-    RouterLink,
-    ListLoadingComponent,
-    FilterComponent,
-    CircleUserIcon,
-    NgbPagination,
-  ],
+  imports: [CommonModule, ReactiveFormsModule, RouterLink, ListContainerComponent, CircleUserIcon],
   templateUrl: './albums.component.html',
   styleUrl: './albums.component.scss',
 })
 export class AlbumsPage implements OnInit {
   private storageService = inject(StorageService);
   private activeRoute = inject(ActivatedRoute);
-  private router = inject(Router);
 
   readonly #destroyRef = inject(DestroyRef);
 
-  loading = true;
-  _currPage = 1;
-  pageSize = 10;
-  totalAlbum = 0;
+  pageState: TPageState = {
+    loading: true,
+    currPage: 1,
+    pageSize: 12,
+    totalContent: 0,
+  };
 
-  search = '';
-  sort: TSortQuery = 'DEFAULT';
+  pagefilter: TPageFilter = {
+    search: '',
+    sort: 'DEFAULT',
+  };
 
-  filteredAlbums = signal<IAlbum[]>([]);
-  albums = computed(() => {
-    const start = (this._currPage - 1) * this.pageSize;
-    const end = start + this.pageSize;
-    return this.filteredAlbums().slice(start, end);
-  });
+  filteredAlbums: IAlbum[] = [];
+  albums: IAlbum[] = [];
 
   ngOnInit(): void {
     this.activeRoute.queryParams.pipe(takeUntilDestroyed(this.#destroyRef)).subscribe({
       next: ({ search, sort, page }) => {
-        this.search = search || '';
-        this.sort = sort || 'DEFAULT';
-        this._currPage = page || 1;
+        this.pagefilter.search = search || '';
+        this.pagefilter.sort = sort || 'DEFAULT';
+        this.pageState.currPage = page || 1;
 
         this.loadAlbums(search, sort);
       },
     });
   }
 
-  get currPage() {
-    return this._currPage;
-  }
-
-  set currPage(page: number) {
-    this._currPage = page;
-    this.applyFilters({ search: this.search, sort: this.sort });
-  }
-
   loadAlbums(query?: string, sort?: string) {
-    this.filteredAlbums.set([...this.storageService.albums]);
+    this.filteredAlbums = [...this.storageService.albums];
 
     if (query) this.searchAlbums(query);
     if (sort) this.sortAlbums(sort as TSortQuery);
 
-    this.totalAlbum = this.filteredAlbums().length;
-    this.loading = false;
+    const start = (this.pageState.currPage - 1) * this.pageState.pageSize;
+    const end = start + this.pageState.pageSize;
+
+    this.albums = this.filteredAlbums.slice(start, end);
+    this.pageState.totalContent = this.filteredAlbums.length;
+    this.pageState.loading = false;
   }
 
   getAlbumAuthor(id: number) {
@@ -83,36 +68,24 @@ export class AlbumsPage implements OnInit {
     return name;
   }
 
-  applyFilters({ search, sort }: TFilter) {
-    this.router.navigate(['albums'], {
-      queryParams: {
-        search: search || undefined,
-        sort: sort === 'DEFAULT' ? undefined : sort,
-        page: this.currPage,
-      },
-    });
-  }
-
   searchAlbums(query: string) {
-    this.filteredAlbums.set(
-      this.storageService.albums.filter((album) => album.title.includes(query)),
-    );
+    this.filteredAlbums = this.storageService.albums.filter((album) => album.title.includes(query));
   }
 
   sortAlbums(sort: TSortQuery) {
     switch (sort) {
       case 'ASC':
-        this.filteredAlbums.update((albums) =>
-          albums.sort((a, b) => a.title[0].localeCompare(b.title[0])),
+        this.filteredAlbums = this.filteredAlbums.sort((a, b) =>
+          a.title[0].localeCompare(b.title[0]),
         );
         break;
       case 'DESC':
-        this.filteredAlbums.update((albums) =>
-          albums.sort((a, b) => b.title[0].localeCompare(a.title[0])),
+        this.filteredAlbums = this.filteredAlbums.sort((a, b) =>
+          b.title[0].localeCompare(a.title[0]),
         );
         break;
       default:
-        this.filteredAlbums.update((albums) => albums.sort((a, b) => a.id - b.id));
+        this.filteredAlbums = this.filteredAlbums.sort((a, b) => a.id - b.id);
         break;
     }
   }
